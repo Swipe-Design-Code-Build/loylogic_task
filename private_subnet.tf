e21f5fe5e7c6a6ad647b2205a0983055925fdc6f
tusharjoshi948@gmail.com <tusharjoshi948@gmail.com>
2021-06-08 20:20:18 +0530

    --- Auto-Git Commit ---

--- private_subnet.tf
+++ private_subnet.tf
@@ -3,8 +3,7 @@ resource "aws_subnet" "private_subnet" {
     aws_vpc.myvpc,
     aws_subnet.private_subnet
   ]
-  
-  # VPC in which subnet has to be created!
+   
   vpc_id = aws_vpc.myvpc.id
    
   cidr_block = "172.20.20.0/24"
