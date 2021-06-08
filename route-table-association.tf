197dab8faa80ea94e7e428637489085d64e8f6a5
tusharjoshi948@gmail.com <tusharjoshi948@gmail.com>
2021-06-08 20:28:00 +0530

    --- Auto-Git Commit ---

--- route-table-association.tf
+++ route-table-association.tf
@@ -4,7 +4,7 @@ resource "aws_route_table_association" "RT-IG-Association" {
     aws_vpc.myvpc,
     aws_subnet.public_subnet,
     aws_subnet.private_subnet,
-    aws_route_table.Public-Subnet-RT
+    aws_route_table.
   ]
 
 # Public Subnet ID
