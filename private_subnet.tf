c89383b0ccacf696b404797797c87893160c545f
tusharjoshi948@gmail.com <tusharjoshi948@gmail.com>
2021-06-08 20:18:57 +0530

    --- Auto-Git Commit ---

---
+++ private_subnet.tf
@@ -0,0 +1,19 @@
+resource "aws_subnet" "subnet2" {
+  depends_on = [
+    aws_vpc.custom,
+    aws_subnet.subnet1
+  ]
+  
+  # VPC in which subnet has to be created!
+  vpc_id = aws_vpc.custom.id
+  
+  # IP Range of this subnet
+  cidr_block = "192.168.1.0/24"
+  
+  # Data Center of this subnet.
+  availability_zone = "ap-south-1b"
+  
+  tags = {
+    Name = "Private Subnet"
+  }
+}
