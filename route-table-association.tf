b2219380fd89d712ed5c0571091ff58854ea4867
tusharjoshi948@gmail.com <tusharjoshi948@gmail.com>
2021-06-08 20:27:29 +0530

    --- Auto-Git Commit ---

---
+++ route-table-association.tf
@@ -0,0 +1,15 @@
+resource "aws_route_table_association" "RT-IG-Association" {
+
+  depends_on = [
+    aws_vpc.custom,
+    aws_subnet.subnet1,
+    aws_subnet.subnet2,
+    aws_route_table.Public-Subnet-RT
+  ]
+
+# Public Subnet ID
+  subnet_id      = aws_subnet.subnet1.id
+
+#  Route Table ID
+  route_table_id = aws_route_table.Public-Subnet-RT.id
+}
