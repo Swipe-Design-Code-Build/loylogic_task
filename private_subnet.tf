ca09b8dca63b6e717a9c0f75c0f138fd7f98df18
tusharjoshi948@gmail.com <tusharjoshi948@gmail.com>
2021-06-08 20:20:07 +0530

    --- Auto-Git Commit ---

--- private_subnet.tf
+++ private_subnet.tf
@@ -6,12 +6,10 @@ resource "aws_subnet" "private_subnet" {
   
   # VPC in which subnet has to be created!
   vpc_id = aws_vpc.myvpc.id
-  
-  # IP Range of this subnet
+   
   cidr_block = "172.20.20.0/24"
-  
-  # Data Center of this subnet.
-  availability_zone = "us-east"
+   
+  availability_zone = "us-east-1b"
   
   tags = {
     Name = "Private Subnet"
