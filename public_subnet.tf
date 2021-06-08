0e895a4adf0ec6a0dd8c6cf9cddd016d79a61b55
tusharjoshi948@gmail.com <tusharjoshi948@gmail.com>
2021-06-08 20:15:26 +0530

    --- Auto-Git Commit ---

--- public_subnet.tf
+++ public_subnet.tf
@@ -2,10 +2,11 @@ resource "aws_subnet" "public_subnet"{
     depends on = [
         aws_vpc.myvpc
     ]
-
- vpc_id = aws_vpc.myvpc.vpc_id
+    vpc_id = aws_vpc.myvpc.vpc_id
     cidr_block = "172.20.10.0/24"
     availability_zone = "us-east-1"
     map_public_ip_on_launch = true
+
+    tags = 
 
 }
